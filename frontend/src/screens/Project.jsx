5dc0f80deb05c916997a4a0d7d301cd1c05bb3f7
Hiten Kalda <hitenkalda@example.com>
2025-08-17 02:58:06 +0530

    Fix: add webcontainer config for frontend build

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useContext, useRef } from 'react'
 import { UserContext } from '../context/user.context'
 import { useNavigate, useLocation } from 'react-router-dom'
-import axios from '../config/axios'
-import { initializeSocket, receiveMessage, sendMessage } from '../config/socket'
+import axios from '../config/axios.js'
+import { initializeSocket, receiveMessage, sendMessage } from '../config/socket.js'
 import Markdown from 'markdown-to-jsx'
 import hljs from 'highlight.js';
-import { getWebContainer } from '../config/webcontainer';
+import { getWebContainer } from '../config/webContainer.js';
 
 
 
